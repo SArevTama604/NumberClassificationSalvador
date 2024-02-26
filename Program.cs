a629a3c52e539d87dafc4a8c863a152cb172af9d
Salvador <sarevalotamayo@student.rtc.edu>
2024-02-25 18:51:21 -0800

    Update Program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > 0)
+            {
+                Console.WriteLine("This is a positive number.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("This is a negative number."); ;
+            }
+            else
+            {
+                Console.WriteLine("This is zero.");
+;            }
         }
     }
 }
